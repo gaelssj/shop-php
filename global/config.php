29d9aa711813a82bee6dc3c7d4d057fe49388e78
Gael <144062863+gaelssj@users.noreply.github.com>
2023-09-06 18:46:15 -0300

    Add files via upload

---
+++ global/config.php
@@ -0,0 +1,17 @@
+<?php
+define("KEY", "");
+define("COD", "AES-128-ECB");
+define("SERVIDOR", "localhost");
+define("USUARIO", "");
+define("PASSWORD", "");
+define("BD", "");
+define("DESCARGASPERMITIDAS","1");
+//SANDBOX (PRUEBA PAYPAL):
+define("LINKAPI","https://api-m.sandbox.paypal.com");
+define("CLIENTID","");
+define("SECRET","");
+//LIVE (REAL PAYPAL):
+// define("LINKAPI","https://api.paypal.com");
+// define("CLIENTID","");  COLOCAR AQUI EL CODIGO LIVE DE CLIENT ID Y COMENTAR LOS CODIGOS DE ARRIBA
+// define("SECRET","");  COLOCAR AQUI EL CODIGO LIVE DE SECRET Y COMENTAR LOS CODIGOS DE ARRIBA
+?>
---
+++ pagar.php
@@ -0,0 +1,72 @@
+<?php
+include 'global/config.php';
+include 'global/conexion.php';
+include 'carrito.php';
+include 'templates/cabecera.php';
+?>
+<?php
+if($_POST) {
+    $total=0;
+    $SID=session_id();
+    $Correo=$_POST['email'];
+    foreach($_SESSION[ 'carrito'] as $i=>$producto) {
+        $total=$total+($producto['PRECIO']*$producto['CANTIDAD']);
+    }
+    $sentencia=$pdo->prepare("INSERT INTO `tblventas` 
+                        (`ID`, `ClaveTransaccion`, `PaypalDatos`, `Fecha`, `Correo`, `Total`, `Status`)
+                VALUES (NULL,:ClaveTransaccion, '', NOW(), :Correo, :Total, 'pendiente');");
+    $sentencia->bindParam(":ClaveTransaccion",$SID);
+    $sentencia->bindParam(":Correo",$Correo);
+    $sentencia->bindParam(":Total",$total);
+    $sentencia->execute();
+    $idVenta=$pdo->lastInsertId();
+    foreach($_SESSION['carrito'] as $i=>$producto) {
+        $sentencia=$pdo->prepare("INSERT INTO `tbldetalleventa`
+                (`ID`, `IDVENTA`, `IDPRODUCTO`, `PRECIOUNITARIO`, `CANTIDAD`, `DESCARGADO`)
+        VALUES (NULL, :IDVENTA, :IDPRODUCTO, :PRECIOUNITARIO, :CANTIDAD, '0');");
+        $sentencia->bindParam(":IDVENTA",$idVenta);
+        $sentencia->bindParam(":IDPRODUCTO",$producto['ID']);
+        $sentencia->bindParam(":PRECIOUNITARIO",$producto['PRECIO']);
+        $sentencia->bindParam(":CANTIDAD",$producto['CANTIDAD']);
+        $sentencia->execute();
+
+    }
+}
+?>
+<!-- Cambiar el ID de Cliente de Paypall :P -->
+<script src="https://www.paypal.com/sdk/js?client-id=CLIENT-ID&currency=USD"></script> 
+<div class="mt-5 p-2 text-center bg-dark text-white rounded">
+    <h1 class="display-4">¡Paso Final!</h1>
+    <hr class="my-4">
+    <p class="lead">Estas a punto de pagar con paypal la cantidad:
+        <h4>$<?php echo number_format($total,2); ?></h4>
+    </p>
+    <div id="paypal-button-container"></div>
+    <p>Los productos podrán ser enviados una vez que se procese el pago.</br>
+        <strong>(Para aclaraciones: hello@gaelst.com)</strong>
+    </p>
+</div>
+<script>
+      paypal.Buttons({
+        createOrder: (data, actions) => {
+          return actions.order.create({
+            purchase_units: [{
+              amount: {
+                value: '<?php echo $total; ?>'
+              },
+              description: "Compra de Mind Shop: $<?php echo number_format($total,2); ?>",
+              reference_id: "<?php echo $SID; ?>#<?php echo openssl_encrypt($idVenta,COD,KEY); ?>"
+            }]
+          });
+        },
+        style: {
+                layout: 'horizontal'
+            },
+        onApprove: (data, actions) => {
+          return actions.order.capture().then(function(orderData) {
+            window.location="verificador.php?facilitatorAccessToken="+data.facilitatorAccessToken+"&orderID="+data.orderID+"&payerID="+data.payerID;
+          });
+        }
+      }).render('#paypal-button-container');
+</script>
+<?php include 'templates/pie.php'; ?>
